f10aad2c87e66f3bd6ea2bb20654d9dc33ffcaba
Nadya-cheb <nadya21cheb@gmail.com>
2023-01-27 11:09:38 +0300

    add code js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,22 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = expr.split('**********');
+    let new_array = arr.map(item => {
+        let subStr = (xs, s) => xs.length ? [xs.slice(0, s), ...subStr(xs.slice(s), s)] : [];
+        let splitItem = item.split('').reverse().join('');
+        let subArray = subStr(splitItem, 10).reverse();
+
+        let arr_new = [];
+        subArray.forEach(element => {
+            element = element.split('').reverse().join('');
+            element = element.replace(/11|10/g, str => str === '11' ? '-' : '.');
+            element = element.replace(/00|0/g, '');
+            arr_new.push(MORSE_TABLE[element]);
+        });
+        return item = arr_new.join('');
+    })
+    return new_array.join(' ');
 }
 
 module.exports = {
